06d9fa638580f28a762fdbc2cf448acc75a0b830
Charlie Eysbroek <C.eysbroek@hotmail.com>
2023-03-07 19:38:17 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <main>
-      Begin hier met de tutorial!
+      <article>
+          <img
+              src="assets/CitroenInEenGlas.jpg"
+              alt="Citroen"
+          />
+          <h2>Citroen</h2>
+          <p>
+              Een citroen is vor de meest mensen te zuur om zo uit de hand te eten.
+              Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het
+              sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.
+          </p>
+      </article>
     </main>
   );
 }
